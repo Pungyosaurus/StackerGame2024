44877cf54490faf0357c77094bd4c1971864f401
Pungyosaurus <jayteewong1@gmail.com>
2024-05-22 17:05:36 -0400

    fasd

--- src/main/Stacker.java
+++ src/main/Stacker.java
@@ -1,10 +1,17 @@
 package main;
 
+import javax.swing.JFrame;
+
 public class Stacker {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("testing pls work");
+		JFrame window = new JFrame();
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setResizable(false);
+		window.setTitle("Construction Crane Chaos");
+		
 	}
 
 }

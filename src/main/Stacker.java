21b4b527da6addda631389f8178627042061f41b
Pungyosaurus <jayteewong1@gmail.com>
2024-05-22 21:11:00 -0400

    asdf

--- src/main/Stacker.java
+++ src/main/Stacker.java
@@ -12,8 +12,6 @@ public class Stacker {
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setResizable(false);
 		window.setTitle("Construction Crane Chaos");
-		window.setLocationRelativeTo(null);
-		window.setVisible(true);
 		
 		GamePanel gamePanel = new GamePanel();
 		window.add(gamePanel);

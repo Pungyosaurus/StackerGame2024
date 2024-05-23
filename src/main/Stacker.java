a0da10f09ed05d8590c7be4ee30fbd02265681ab
Pungyosaurus <jayteewong1@gmail.com>
2024-05-22 20:39:31 -0400

    waht

--- src/main/Stacker.java
+++ src/main/Stacker.java
@@ -6,7 +6,7 @@ public class Stacker {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("kalsdfj;lkasdjfsk davebettersersedfsdfsffs");
+
 		JFrame window = new JFrame();
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setResizable(false);

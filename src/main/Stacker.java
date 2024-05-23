df1185cb6855a4b4faaee3d78766db4d50a405a1
Pungyosaurus <jayteewong1@gmail.com>
2024-05-22 20:59:35 -0400

    done by pungy

--- src/main/Stacker.java
+++ src/main/Stacker.java
@@ -6,8 +6,7 @@ public class Stacker {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("kalsdfj;lkasdjfsk");
-		System.out.println("sfdfsdfsdfsdfsdf");
+
 
 		JFrame window = new JFrame();
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);

d92b074a70a45c930e959d5c89a940ceced935fe
Pungyosaurus <jayteewong1@gmail.com>
2024-05-23 21:23:56 -0400

    new crane fields

--- src/entities/Crane.java
+++ src/entities/Crane.java
@@ -1,5 +1,22 @@
 package entities;
 
-public class Crane {
+import java.awt.image.BufferedImage;
+
+public class Crane extends GameObject {
+
+	public Crane(int w, int h, BufferedImage image) {
+		super(w, h, image);
+		BufferedImage resizedCrane = resizeBuffImage(image, w, h);
+        setSprite(resizedCrane);
+		
+	}
+	
+	private int connectX, connectY;
+
+	@Override
+	public void act() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }

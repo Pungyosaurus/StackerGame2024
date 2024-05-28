903e587a8c026efbf7dbeeb76df9b95cb519a2ab
Pungyosaurus <jayteewong1@gmail.com>
2024-05-27 20:38:56 -0400

    FINALLY GOT CABLE WORKING (CLIP) and resizing the image based on the direction (Needs to be smoother)

--- src/entities/Cable.java
+++ src/entities/Cable.java
@@ -1,54 +1,84 @@
 package entities;
 
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 
 public class Cable extends GameObject {
-	
-	private final double MAX_ANGLE = 0.785398163397449 + 0.7689920684287006
-;
-	private final double MIN_ANGLE = -0.9599310885968821 + 0.7689920684287006
-;
-	private final int WIDTH;
-	// zero is to the left (adding), one is to the right (subtracting)
+
+	// zero is moving to the left, 1 is moving to the right
 	private boolean direction;
-	private double da = 49;
+	private double da = 50;
 	private final double d2a = 2;
-	
 
 	public Cable(int x, int y, int w, int h, BufferedImage image) {
 		super(x, y, w, h, image);
-		this.WIDTH = w;
 		spriteAngle = Math.PI / 4;
 	}
 
 	@Override
 	public void act() {
-		
-		if(da < 0) {
-			if(spriteAngle < Math.PI / 4) 
+
+		if (da < 0) {
+			// going right
+			direction = true;
+			if (spriteAngle < Math.PI / 4)
+				// going to the right and past 90 (slowing down)
 				da += d2a;
 			else
+				// going to the right and not past 90 (speeding up)
 				da -= d2a;
-		}else if(da > 0) {
-			if(spriteAngle > Math.PI / 4) 
+		} else if (da > 0) {
+			// going left
+			direction = false;
+			if (spriteAngle > Math.PI / 4)
+				// going to the left and past 90 (slowing down)
 				da -= d2a;
+
 			else
+				// going to the left and not past 90 (speeding up)
 				da += d2a;
-		}
-		
-		
-		
-		
+		} else if (!direction) // cases when da = 0;
+			da -= d2a;
+		else
+			da += d2a;
+
+		if (direction)
+			setSize(getWidth() + (int)(da / 5), getHeight() + (int)(da / 5));
+		else
+			setSize(getWidth() + (int)(da / 5), getHeight() + (int)(da / 5));
 		spriteAngle += Math.toRadians(da / 10);
 
-		
-		
-		
-		
-
-		
 	}
-	
+
+	@Override
+	public void paintComponent(Graphics g) {
+
+		Graphics2D g2d = (Graphics2D) g;
+		try {
+			if (spriteAngle != 0) {
+
+				// Calculate the bounding box of the rotated object
+				AffineTransform rotation = AffineTransform.getRotateInstance(spriteAngle);
+				Rectangle rotatedBounds = rotation
+						.createTransformedShape(new Rectangle(0, 0, sprite.getWidth(), sprite.getHeight())).getBounds();
+
+				// KEY CHANGE
+				g2d.setClip(rotatedBounds);
+
+				// Draw the rotated image
+				g2d.rotate(spriteAngle);
+				g2d.drawImage(sprite, 0, 0, getWidth(), getHeight(), this);
+			}
+		} catch (Exception e) {
+			g.drawImage(sprite, 0, 0, getWidth(), getHeight(), this);
+
+		}
+
+	}
+
 	public double getSpriteAngle() {
 		return this.spriteAngle;
 	}

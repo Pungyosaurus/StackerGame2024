4cf62dda16169505fd11c0042aaf89399f5f178d
Dave <davinder2007@gmail.com>
2024-06-06 23:12:03 -0400

    CUT IS ALMOST DONE, JUST NEED TO ALTER SIDE CUT VALUES

--- src/main/Stacker.java
+++ src/main/Stacker.java
@@ -93,7 +93,7 @@ public class Stacker extends GamePanel {
 		BuildingCut temp = new BuildingCut(1000,500,groundWidth*7,groundHeight*7,iGround);
 		add(temp);
 		
-		temp.cut(0, 0, 90, 0);
+		temp.cut(30,50,20,20);
 		
 		
 		repaint();
@@ -103,7 +103,6 @@ public class Stacker extends GamePanel {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}		
-		temp.cut(0, 0, 40, 0);
 
 //		temp.cut(0, 0);
 //		temp.setSize(70, temp.getWidth());
